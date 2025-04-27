ad4dcbe33a7a20988d52572d7eddb86a73e9e355
ChrisChamo5 <chrischamora55@gmail.com>
2025-04-27 17:42:27 -0600

    Inicio cc

---
+++ Proyecto 1/JuegoAhorcado.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class JuegoAhorcado {
+    //Atributos
+    private String palabraSecreta; // Palabra que el jugador debe adivinar
+    private char [] palabraOculta; // Representación con guiones(_ _ _)
+    private int intentosRestantes; //Número de intentos disponibles
+    private String letrasUsadas; // Letras que el Jugador ya intentó
+
+
+
+
+
+
+
+
+}
