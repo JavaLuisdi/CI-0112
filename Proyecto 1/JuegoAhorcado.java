d1af2527c9921baf73d88aa7aa3de94089e8c35f
ChrisChamo5 <chrischamora55@gmail.com>
2025-04-27 18:01:31 -0600

    chrischamooooo top

--- Proyecto 1/JuegoAhorcado.java
+++ Proyecto 1/JuegoAhorcado.java
@@ -6,7 +6,7 @@ public class JuegoAhorcado {
     private char [] palabraOculta; // Representación con guiones(_ _ _)
     private int intentosRestantes; //Número de intentos disponibles
     private String letrasUsadas; // Letras que el Jugador ya intentó
-
+jbkjbkbjkb
 
 
 
