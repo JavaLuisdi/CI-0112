b56cddcebfb90da2007accbbd0805d02fdf20691
ChrisChamo5 <chrischamora55@gmail.com>
2025-05-11 18:44:02 -0600

    MainAhorcado

--- Proyecto 1/JuegoAhorcado.java
+++ Proyecto 1/JuegoAhorcado.java
@@ -1,79 +1,59 @@
-public class JuegoAhorcado{
+public class JuegoAhorcado {
+    private String palabraSecreta;
+    private char[] palabraAdivinada;
+    private int intentosRestantes;
+    private boolean juegoTerminado;
 
-private char [] palabraOculta; // Representación con guiones(_ _ _)
-private int intentosRestantes; //Número de intentos disponibles
-private String letrasUsadas; // Letras que el Jugador ya intentó
-
-
-
-
-
-    public JuegoAhorcado() {
-        // Inicializa valores por defecto 
-        this.palabraSecreta = "";
-        this.palabraOculta = new char[0];
-        this.intentosRestantes = 6;
-        this.letrasUsadas = "";
-    }
-    // Inicializa el juego con una nueva palabra secreta
-    public void inicializarJuego(String palabra){
-        this.palabraSecreta = palabra.toLowercase();
-        this.palabraOculta = new char[palabra.length()];
-        Arrays.fill(this.palabraOculta,'_'); // Llena el arreglo con _
-        this.intentosRestantes = 6;
-        this.letrasUsadas = "";
-    }
-    // Procesa una letra ingresada por el jugador 
-   public boolean procesarLetra(char letra){
-       letra = Character.toLowercase(letra);// Convertir a minúscula para evitar problemas
-      
-      // Si la letra ya fue usada, no hacer nada 
-      if (letrasUsadas.contains(String.valueOf(letra))){
-         return false; 
-      }
-      //Registrar la letra como usada
-      letrasUsadas += letra + " ";
-      
-      boolean letraAdivinada = false;
-
-      //Buscar la letra en la palabra secreta 
-      for(int i = 0; i < palabraSecreta.length(); i++){
-        if (palabraSecreta.charAt(i) == letra){
-            palabraOculta[i] = letra; // Revelar la letra en su posición
-            letraAdivinada = true;
+    public JuegoAhorcado(String palabraSecreta) {
+        this.palabraSecreta = palabraSecreta.toUpperCase();
+        this.palabraAdivinada = new char[palabraSecreta.length()];
+        for (int i = 0; i < palabraAdivinada.length; i++) {
+            palabraAdivinada[i] = '_';
         }
-      }
-      // Si no se adivinó, restar un intento 
-      if (!letraAdivinada) {
-        intentosRestantes--;
-      }
-      return letraAdivinada;
-   }
-// Verificar si el jugador ha adivinado toda la palabra
-public boolean esVictoria(){
-    return String.valueOf(palabraOculta).equals(palabraSecreta);
-      }
-// Verificar si el jugador se quedó sin intentos
-public boolean esDerrota(){
-    return intentosRestantes <= 0;
-}
-// Obtiene la palabra oculta en formato String
+        this.intentosRestantes = 6;
+        this.juegoTerminado = false;
+    }
 
-public String getPalabraOculta() {
-    return String.valueOf(palabraOculta);
-}
-// Obtiene las letras que ya se han intentado 
-public String getLetrasUsadas(){
-    return letrasUsadas;
-}
-// Obtiene los intentos restantes
-public int getIntentosRestantes(){
-    return intentosRestantes;
-}
-// Obtiene la palabra secreta
-public String getPalabraSecreta() {
-    return palabraSecreta;
-}
+    // Verifica si la letra está en la palabra secreta
+    public boolean adivinarLetra(char letra) {
+        boolean acierto = false;
+        for (int i = 0; i < palabraSecreta.length(); i++) {
+            if (palabraSecreta.charAt(i) == letra) {
+                palabraAdivinada[i] = letra;
+                acierto = true;
+            }
+        }
+        if (!acierto) {
+            intentosRestantes--;
+        }
+        actualizarEstadoJuego();
+        return acierto;
+    }
 
+    // Actualiza el estado del juego (si ganó o perdió)
+    private void actualizarEstadoJuego() {
+        if (intentosRestantes <= 0) {
+            juegoTerminado = true;
+        } else if (String.valueOf(palabraAdivinada).equals(palabraSecreta)) {
+            juegoTerminado = true;
+        }
+    }
 
+    // Muestra el progreso actual (ej: J _ V _)
+    public String getProgreso() {
+        return String.valueOf(palabraAdivinada);
+    }
+
+    // Getters para acceder a los atributos
+    public int getIntentosRestantes() {
+        return intentosRestantes;
+    }
+
+    public boolean isJuegoTerminado() {
+        return juegoTerminado;
+    }
+
+    public String getPalabraSecreta() {
+        return palabraSecreta;
+    }
 }
