6448c4bed1bb5b44829cbbf092721037ca4ec2e9
ChrisChamo5 <chrischamora55@gmail.com>
2025-05-16 01:22:50 -0600

    ReadMe

--- Proyecto1/Barco.java
+++ Proyecto1/Barco.java
@@ -1,34 +1,72 @@
 public class Barco {
-    //Atributos de cada barco
+    /**
+     * @brief Atributos de cada barco
+     */
     int row;
     int column;
     boolean sinked = false;
     boolean attacked = false;
 
-    //Métodos set para crear y editar un barco
+    /**
+     * @brief Métodos set para crear y editar un barco
+     * @param row Fila donde se encuentra el barco
+     */
     public void setRow(int row) {
         this.row = row;
     }
+
+    /**
+     * @brief Establece la columna del barco
+     * @param column Columna donde se encuentra el barco
+     */
     public void setColumn(int column) {
         this.column = column;
     }
+
+    /**
+     * @brief Establece si el barco ha sido hundido
+     * @param sinked Estado de hundimiento del barco
+     */
     public void setSinked(boolean sinked) {
         this.sinked = sinked;
     }
+
+    /**
+     * @brief Establece si el barco ha sido atacado
+     * @param attacked Estado de ataque del barco
+     */
     public void setAttacked(boolean attacked) {
-        this.attacked = attacked;
+        this.attacked = this.attacked;
     }
 
-    //Métodos get para devolver atributos de un barco
+    /**
+     * @brief Métodos get para devolver atributos de un barco
+     * @return La fila donde se encuentra el barco
+     */
     public int getRow() {
         return row;
     }
+
+    /**
+     * @brief Devuelve la columna donde se encuentra el barco
+     * @return La columna del barco
+     */
     public int getColumn() {
         return column;
     }
+
+    /**
+     * @brief Indica si el barco ha sido hundido
+     * @return true si el barco está hundido, false en caso contrario
+     */
     public boolean getSinked() {
         return sinked;
     }
+
+    /**
+     * @brief Indica si el barco ha sido atacado
+     * @return true si el barco ha sido atacado, false en caso contrario
+     */
     public boolean getAttacked() {
         return attacked;
     }
